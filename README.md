186dcce0fdf3a958afbc8fd2b838607b8b5b6f86
me <me@me.me.com>
2020-04-26 01:55:14 -0700

    and/fillout a readme.md file

---
+++ README.md
@@ -0,0 +1,31 @@
+# _Word Order_
+
+#### _This demonstrates knowledge of JavaScript._
+
+#### By _**Matthew LeDoux**_
+
+## Description
+
+_A page which demonstrates beginner JavaScript with filter,map, and reduce._
+
+## Setup/Installation Requirements
+
+Clone the repo and open the .html file in a browser.
+
+## Known Bugs
+
+_I don't think there are any bugs but please tell me if you see any._
+
+## Support and contact details
+
+_Open a Github issue if you see a problem or have questions_
+
+## Technologies Used
+
+_This is an html, css, Bootstrap, jQuery, and JavaScript thing._
+
+### License
+
+*Feel free to use this code as you please*
+
+Copyright (c) 2020 **_Matthew LeDoux_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
 function processRawSentence(rawSentence){
-  var words = rawSentence.split(" ").map(x => x.trim());
-  console.log(words)
+  var words = rawSentence.split(".").join(" ").split(" ").map(x => x.trim());
+  
   var uniqueWords = words.reduce(function(accumulator,currentValue){
     if (accumulator.map(x => Object.keys(x)[0]).includes(currentValue)){
       return accumulator;
     }
     var count = words.filter(x => x === currentValue).length;
     
-    var newAccumulator = accumulator.concat([{currentValue:count}]);
+    var newAccumulator = accumulator.concat([{[currentValue]:count}]);
     return newAccumulator;
   },[]);
+  
   var orderedUniqueWords = uniqueWords.reduce(function(accumulator,currentValue){
-    var highest = Math.max(...[uniqueWords.map(x => x[Object.keys(x)[0]])]);
+    var numberArray = uniqueWords.map(x => x[Object.keys(x)[0]]);
+    var filteredNumberArray = numberArray.filter(function(number){
+      return !accumulator.map(x => x[Object.keys(x)[0]]).includes(number);
+    });
+    var highest = Math.max(...filteredNumberArray);
+    
     var highestNumbers = uniqueWords.filter(x =>  x[Object.keys(x)[0]] === highest);
-    return accumulator.concat(highestNumbers);
+    var newAccumulator = accumulator.concat(highestNumbers);
+    return newAccumulator;
   },[]);
-  console.log(orderedUniqueWords)
+ 
   return orderedUniqueWords;
 }
 
@@ -23,12 +30,15 @@ function processRawSentence(rawSentence){
 $(document).ready(function(){
   $("form").submit(function(e){
     e.preventDefault();
-    
+    clear();
     var rawSentence = $("textarea").val();
     
     var uniqueWords = processRawSentence(rawSentence);
-    Object.keys(uniqueWords).forEach(function(word){
+    uniqueWords.forEach(function(word){
       $("ul").append("<li>"+ Object.keys(word)[0] + " : " + word[Object.keys(word)[0]]  +"</li>")
-    })
+    });
+    function clear(){
+      $("li").remove();
+    }
   });
 });
